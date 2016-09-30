70806797ae5c9719ce1f09646f504deb45064d40
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-09-30 16:26:09 +0100

    detect an un-caught exception
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/goto-symex/symex_throw.cpp
+++ src/goto-symex/symex_throw.cpp
@@ -78,7 +78,18 @@ bool goto_symext::symex_throw(statet &state)
       return true;
     }
     else // We don't have a catch for it yet
-      assert(0);
+    {
+      // An un-caught exception.
+      const std::string &msg="Throwing an exception of type " +
+            exceptions_thrown.begin()->id_string() +
+            " but there is not catch for it.";
+
+      // Generate an assertion to fail
+      vcc(false_exprt(), msg, state);
+
+      // Behaves like assume(0);
+      symex_assume(state, false_exprt());
+    }
   }
 
   return false;
