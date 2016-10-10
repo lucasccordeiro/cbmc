898b7199bfbed5b7a18a0628978941568542f72d
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-10-10 16:16:10 +0100

    add message 'catches all uncaught exceptions'
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/cbmc/cbmc_parse_options.cpp
+++ src/cbmc/cbmc_parse_options.cpp
@@ -1140,7 +1140,7 @@ void cbmc_parse_optionst::help()
     " --round-to-minus-inf         rounding towards minus infinity\n"
     " --round-to-zero              rounding towards zero\n"
     " --function name              set main function name\n"
-    " --try-catch                  add try-catch block\n"
+    " --try-catch                  catches all uncaught exceptions\n"
     "\n"
     "Program representations:\n"
     " --show-parse-tree            show parse tree\n"
